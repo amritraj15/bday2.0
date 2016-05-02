a7e57ac453c483ba5e51fab7cc316c67305f8ae6
amritraj <amritraj1510@gmail.com>
2016-05-03 00:08:28 +0530

    Fourth Test

--- mail/mail.php
+++ mail/mail.php
@@ -5,20 +5,8 @@ use Postmark\PostmarkClient;
 
 // Example request
 $client = new PostmarkClient("664bb426-d53c-4abc-a480-d3c4864ec686");
-/*
-$sendResult = $client->sendEmail(
-  600961,
-  [
-  "product_name" => "product_name_Value",
-  "name" => "name_Value",
-  "action_url" => "action_url_Value",
-  "username" => "username_Value",
-  "sender_name" => "sender_name_Value",
-  "product_address_line1" => "product_address_line1_Value",
-  "product_address_line2" => "product_address_line2_Value",
-]);
-*/
-/*
+
+
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
@@ -32,12 +20,6 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-*/
-$name = "Amrit Raj 1";
-$email_address = "amritraj1015@gmail.com";
-$phone ="9876543210";
-$message = "Hi there";
-
 
 // Make a request
 
