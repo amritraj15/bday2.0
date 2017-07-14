b1ce072d01aaf7ab2a468ede5848d1f6c6979698
amritraj <amritraj1510@gmail.com>
2017-07-15 01:06:22 +0530

    take1

--- js/effects.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#underhide").hide();
-  setTimeout(function() {
-    $('#hide').fadeOut("fast");
-  }, 20000);
-  
-   $("#underhide").delay(21000).fadeIn("fast");
-});
-
